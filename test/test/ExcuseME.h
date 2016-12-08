314c65a4c3c354afff440b7f7f1f2249d1b29c89
ykRyan <RyanHoo@3bz53-h01-176-130-11-23.dsl.sta.abo.bbox.fr>
2016-12-08 15:25:06 +0800

    edit

---
+++ test/test/ExcuseME.h
@@ -0,0 +1,13 @@
+//
+//  ExcuseME.h
+//  test
+//
+//  Created by 小王 on 2016/12/8.
+//  Copyright © 2016年 小王. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExcuseME : NSObject
+
+@end
---
+++ test/test/ExcuseME.m
@@ -0,0 +1,13 @@
+//
+//  ExcuseME.m
+//  test
+//
+//  Created by 小王 on 2016/12/8.
+//  Copyright © 2016年 小王. All rights reserved.
+//
+
+#import "ExcuseME.h"
+
+@implementation ExcuseME
+
+@end
